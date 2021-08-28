68e5ceaf8752071f8fe560a741af63139714ab03
SMei201900 <smei201900@gmail.com>
2021-08-28 16:20:50 -0400

    Added Github Link

--- src/Credits.js
+++ src/Credits.js
@@ -3,13 +3,17 @@ import React from "react";
 export default function Credits() {
 	return (
 		<div>
-			<span class="credit">
-				<a class="code-source" href="/" target="_blank">
+			<span className="credit">
+				<a
+					className="code-source"
+					href="https://github.com/SMei201900/reactwfourhw"
+					target="_blank"
+				>
 					Open source code{" "}
 				</a>
 				by:
 				<a
-					class="profile"
+					className="profile"
 					href="https://www.linkedin.com/in/sally-mei-1a4439210/"
 					target="_blank"
 				>
